63a72b7a4fe06e948f0b6f2ef0511e5e3bf50e69
sandygithub101 <kumar.sandeep101162@gmail.com>
2026-05-23 15:17:06 +0530

    Updates

--- client/assets/ezydeal-01A4n6KR.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/sociocharge-BhcODfeF.png",e="/assets/lablive0-DZF5nU2f.png",a="/assets/ezydeal-CtL-gCeU.png";export{a as e,e as l,s};
--- server/assets/ezydeal-ByiDK7qP.js
+++
@@ -1,8 +0,0 @@
-const socio = "/assets/sociocharge-BhcODfeF.png";
-const lablive = "/assets/lablive0-DZF5nU2f.png";
-const ezyMoney = "/assets/ezydeal-CtL-gCeU.png";
-export {
-  ezyMoney as e,
-  lablive as l,
-  socio as s
-};
